f333f946777b4d6f38ff3d3bd4c9edea518482eb
ZaidKhan26 <zzzaaaiiiddd044@gmail.com>
2026-04-28 12:10:01 +0530

    add error middleware and register in server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 const helmet = require("helmet")
 const connectDB = require('./config/db');
+const errorMiddleware = require('./middleware/error.middleware')
 
 connectDB();
 const port = process.env.PORT || 5000;
@@ -19,6 +20,7 @@ app.use(cors({
 app.get("/", (req,res) => {
     res.send("Backend is Running");
 })
+app.use(errorMiddleware)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
